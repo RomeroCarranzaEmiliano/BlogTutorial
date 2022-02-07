f2568e01f634da2eba9ca06631971232aac4feff
RomeroCarranzaEmiliano <emilianoromerocarranza@gmail.com>
2022-02-06 21:21:24 -0300

    Chapter 4: Database

--- microblog.py
+++ microblog.py
@@ -2,5 +2,9 @@
     microblog.py
 """
 
-from app import app
+from app import app, db
+from app.models import User, Post
 
+@app.shell_context_processor
+def make_shell_context():
+    return {"db":db, "User":User, "Poset": Post}
